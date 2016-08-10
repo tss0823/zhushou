5130a5ef5a67dfddf9ab92be1574cf4f7e82d352
shengshan.tang <shengshan.tang@git.yuntaohb.net>
2016-08-11 00:58:51 +0800

    i

--- zhushou-service/src/main/java/com/yuntao/zhushou/service/job/CheckServerStatusJob.java
+++ zhushou-service/src/main/java/com/yuntao/zhushou/service/job/CheckServerStatusJob.java
@@ -16,7 +16,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -47,40 +46,45 @@ public class CheckServerStatusJob {
     @Scheduled(cron = "*/5 * * * * ?")
 //    @Profile("prod")
     public void check() {
-        if(!execRun.compareAndSet(false,true)){
+        if (!execRun.compareAndSet(false, true)) {
             return;
         }
-        try{
+        try {
             String model = AppConfigUtils.getValue("profiles.active");
             if (model.equals(LogModel.TEST.getCode())) {
                 return;
             }
             List<App> appList = appService.selectAllList();
-            Map<String,Map<String,ResponseObject>> resultMap = new HashMap<>();
+            Map<String, Map<String, ResponseObject>> resultMap = new HashMap<>();
             for (App app : appList) {
                 List<Host> hosts = hostService.selectListByAppAndModel(app.getId(), model);
-                Map<String,ResponseObject> statusMap = new HashMap<>();
+                Map<String, ResponseObject> statusMap = new HashMap<>();
                 for (Host host : hosts) {
-                    String checkUrl = "http://"+host.getEth0()+":"+app.getPort()+"/checkServerStatus" ;
-                    List<String> lines = HttpUtils.reqGet(checkUrl);
-                    String result = StringUtils.join(lines,",");
+                    String checkUrl = "http://" + host.getEth0() + ":" + app.getPort() + "/checkServerStatus";
+                    String result = "";
+                    try {
+                        List<String> lines = HttpUtils.reqGet(checkUrl);
+                        result = StringUtils.join(lines, ",");
+                    } catch (Exception e) {
+                        result = e.getMessage();
+                    }
                     boolean checkServerStatusIsOK = StringUtils.equals(result, "checkServerStatusIsOK");
                     ResponseObject responseObject = ResponseObjectUtils.buildResObject();
                     responseObject.setSuccess(checkServerStatusIsOK);
                     responseObject.setMessage(result);
-                    statusMap.put(host.getName(),responseObject);
+                    statusMap.put(host.getName(), responseObject);
 
                 }
-                resultMap.put(app.getName(),statusMap);
+                resultMap.put(app.getName(), statusMap);
             }
             String result = JsonUtils.object2Json(resultMap);
 
             //推送到前端
-            ytWebSocketServer.sendMessage(AppConstant.ResponseType.SERVER_STATUS_CHECK,result);
-        }catch (Exception e){
-            log.error("checkServerError",e);
-            ytWebSocketServer.sendMessage(AppConstant.ResponseType.SERVER_STATUS_CHECK,"error,message="+e.getMessage());
-        }finally {
+            ytWebSocketServer.sendMessage(AppConstant.ResponseType.SERVER_STATUS_CHECK, result);
+        } catch (Exception e) {
+            log.error("checkServerError", e);
+            ytWebSocketServer.sendMessage(AppConstant.ResponseType.SERVER_STATUS_CHECK, "error,message=" + e.getMessage());
+        } finally {
             execRun.set(false);
         }
 
